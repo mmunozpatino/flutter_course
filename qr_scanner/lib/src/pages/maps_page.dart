b2dea1a0ea91d0c4b31fa9118d981679ad7b4529
mmunozpatinomeli <mercedes.munozpatino@mercadolibre.com>
2020-10-15 21:47:11 -0300

    video 160 ok

--- qr_scanner/lib/src/pages/maps_page.dart
+++ qr_scanner/lib/src/pages/maps_page.dart
@@ -7,30 +7,53 @@ class MapsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return FutureBuilder<List<ScanModel>>(
-      future: DbProvider.instance.getAllScan(),
-      builder: (BuildContext context, AsyncSnapshot<List<ScanModel>> snapshot) {
+        future: DbProvider.instance.getAllScan(),
+        builder:
+            (BuildContext context, AsyncSnapshot<List<ScanModel>> snapshot) {
+          if (!snapshot.hasData) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
 
-        if( !snapshot.hasData ) {
-          return Center(
-            child: CircularProgressIndicator(),
+          final scans = snapshot.data;
+
+          if (scans.length == 0) {
+            return Center(
+              child: Text('No hay información'),
+            );
+          }
+
+          return ListView.builder(
+            itemCount: scans.length,
+            itemBuilder: (context, i) => Dismissible(
+              key: UniqueKey(),
+              child: ListTile(
+                leading: Icon(Icons.cloud_queue,
+                    color: Theme.of(context).primaryColor),
+                title: Text(scans[i].value),
+                subtitle: Text(' ID ${scans[i].id}'),
+                trailing: Icon(Icons.keyboard_arrow_right, color: Colors.grey),
+              ),
+              background: Container(
+                color: Colors.red,
+                child: Icon(Icons.remove_circle_outline),
+                alignment: Alignment.centerLeft,
+                padding: EdgeInsets.all(15.0),
+              ),
+              secondaryBackground: Container(
+                color: Colors.green,
+                child: Icon(Icons.check_circle_outline),
+                alignment: Alignment.centerRight,
+                padding: EdgeInsets.all(15.0),
+              ),
+              onDismissed: (DismissDirection direction) {
+                if(direction == DismissDirection.startToEnd) {
+                  DbProvider.instance.deleteScan(scans[i].id);
+                }
+              },
+            ),
           );
-        }
-
-        final scans = snapshot.data;
-
-        if(scans.length == 0) {
-          return Center(child: Text('No hay información'),);
-        } 
-
-        return ListView.builder(
-          itemCount: scans.length,
-          itemBuilder: (context, i) => ListTile(
-            leading: Icon(Icons.cloud_queue, color: Theme.of(context).primaryColor),
-            title: Text(scans[i].value),
-            trailing: Icon(Icons.keyboard_arrow_right ,color: Colors.grey),
-          ),
-        );
-      }
-    );
+        });
   }
 }
