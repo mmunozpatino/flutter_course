4a05fd58720590523fe3b099705a49d02e11dc79
mmunozpatinomeli <mercedes.munozpatino@mercadolibre.com>
2020-10-18 18:12:03 -0300

    video 162 ok

---
+++ qr_scanner/lib/src/bloc/scan_bloc.dart
@@ -0,0 +1,39 @@
+import 'dart:async';
+
+import 'package:qr_scanner/src/providers/db_provider.dart';
+
+class ScanBloc{
+  static final ScanBloc _singleton = ScanBloc._();
+
+  factory ScanBloc() {
+    return _singleton;
+  }
+
+  ScanBloc._(){
+    //scans de la DB
+    getScans();
+  }
+
+  final _scanStreamController = StreamController<List<ScanModel>>.broadcast();
+
+  Stream<List<ScanModel>> get scanStream => _scanStreamController.stream;
+
+
+  dispose() {
+    _scanStreamController?.close(); //funciona como el ? de kotlin
+  }
+
+  getScans() async {
+    _scanStreamController.sink.add(await DbProvider.instance.getAllScan());
+  }
+
+  deleteScan(int id) async{
+    await DbProvider.instance.deleteScan(id);
+    getScans(); //update the list
+  }
+
+  deleteAllScans() async{
+    await DbProvider.instance.deleteAll();
+    getScans(); //update the list
+  }
+}
